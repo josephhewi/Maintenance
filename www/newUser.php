4bce5ae328702f1a5fd10b0df78cfd243dca6d07
Joe Hewitt <70499731+josephhewi@users.noreply.github.com>
2023-01-23 20:44:15 -0600

    Create newUser.php

---
+++ www/newUser.php
@@ -0,0 +1,21 @@
+<!DOCTYPE HTML>
+<head>
+  <title>Add New User</title>
+  <link rel="stylesheet" href="./styles/default.css">
+</head>
+<body>
+  <header>
+  </header>
+  <div>
+    <form action="adduser.php" method="post">
+      <label for="fname">First Name: </label><input type="text" id="fname" name="fname"><br/>
+      <label for="lname">Last Name: </label><input type="text" id="lname" name="lname"><br/>
+      <label for="user">User Name: </label><input type="text" id="user" name="user"><br/>
+      <label for="pwd">Password: </label><input type="password" id="pwd" name="pwd"><br/>
+      <input type="submit"/>
+    </form>
+  </div>
+  <footer>
+  </footer>
+</body>
+</html>
